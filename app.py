9fcddb6efa45324b063f2deb198bc2c5536a8cd2
Kafonek <kafonek@gmail.com>
2017-02-11 13:30:01 -0500

    documentation and more examples

--- app.py
+++ app.py
@@ -7,17 +7,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    return render_template('index.html')
 
 @app.route('/query')
 def query():
     #simulate a query and return some json data
     #time.sleep(random.random() * 10) # sleep 1-10 seconds
-    from bokeh.sampledata.iris import flowers # pandas dataframe of iris 
+    from bokeh.sampledata.iris import flowers # pandas dataframe of iris sample (150 rows x 4 columns)
+    # Add our own 'color' column on the server side because it's easier than doing it in javascript
     colors = dict(zip(flowers.species.unique(), ['blue', 'green', 'red']))
     flowers['color'] = flowers.apply(lambda rec: colors[rec.species], axis=1)
-    data = flowers.to_dict('records') # convert from dataframe to list of dicts
-    return json.dumps(data)
+    return flowers.to_json(orient='records') # return JSON in the form of a list of dictionaries
 
 @app.route('/example1')
 def example1():
@@ -29,8 +29,8 @@ def example2():
 
 @app.route('/<path>')
 def catch_all(path):
-    # This catches anything that doesn't have an explicit match
-    # so using this instead of continuing with the /example3 -> render_template('example3.html') etc etc.
+    # This is a way to set dynamic rules, more info: http://flask.pocoo.org/docs/0.12/quickstart/#routing
+    # instead of manually adding /example3 -> render_template('example3.html'), etc, etc
     html_file = '%s.html' % path # i.e. a request to /example5 would look for 'example5.html'
     return render_template(html_file)
 
